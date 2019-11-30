e1985e3ff95ac2262efef9c4a58c1c299d061830
prostomarkeloff <somefuckingidiotfrominternet@pm.me>
2019-11-30 19:25:37 +0300

    add final API

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import HTTPException
 from db import PhraseInput
 from db import PhraseOutput
 from db import Database
-
-class DeletePhraseResponse(BaseModel):
-    success: bool
 
 app = FastAPI(title="Random phrase")
 db = Database()
 
 @app.get("/get", response_description="Random phrase", description="Get random phrase from database", response_model=PhraseOutput)
 async def get():
-    phrase = db.get(db.get_random())
+    try:
+        phrase = db.get(db.get_random())
+    except IndexError:
+        raise HTTPException(404, "Phrase list is empty")
     return phrase
 
 @app.post("/add", response_description="Added phrase with *id* parameter", response_model=PhraseOutput)
@@ -20,12 +20,9 @@ async def add(phrase: PhraseInput):
     phrase_out = db.add(phrase)
     return phrase_out
 
-@app.delete("/delete", response_description="Result of deleting", response_model=DeletePhraseResponse)
+@app.delete("/delete", response_description="Result of deleting")
 async def delete(id: int):
     try:
         db.delete(id)
-        response = DeletePhraseResponse(success=True)
-    except ValueError:
-        response = DeletePhraseResponse(success=False)
-
-    return response
+    except ValueError as e:
+        raise HTTPException(404, str(e))
